6dbee52dc3a9c22ea8f1dd302b779753aff584cf
andrewtyshkovets <andrew.tyshkovets@gmail.com>
2021-01-02 22:52:34 +0200

    bot response added

--- src/main/java/com/bratva/droidBot/botAPI/DroidBot.java
+++ src/main/java/com/bratva/droidBot/botAPI/DroidBot.java
@@ -6,6 +6,7 @@ import org.telegram.telegrambots.bots.TelegramWebhookBot;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Setter
 @AllArgsConstructor
@@ -32,6 +33,11 @@ public class DroidBot extends TelegramWebhookBot {
     @Override
     public BotApiMethod<?> onWebhookUpdateReceived(Update update) {
         SendMessage replyMessageToUser = new SendMessage(update.getMessage().getChatId(),"Hi "+ update.getMessage().getAuthorSignature());
+        try {
+            execute(replyMessageToUser);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
         return replyMessageToUser;
     }
 
